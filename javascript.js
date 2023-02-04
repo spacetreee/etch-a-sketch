84782fb8525223a0d9540aa6de7fff62a8b60981
Emma <111027901+spacetreee@users.noreply.github.com>
2023-02-03 19:29:12 -0500

    Add mouseover event to divs

--- javascript.js
+++ javascript.js
@@ -1,9 +1,20 @@
 const container = document.querySelector('.container');
+const divs = document.createElement('div');
+
 
 let gridSize = 4;
 for (i = 0; i < gridSize; i++) {
     for (j = 0; j < gridSize; j++) {
-        const div = document.createElement('div');
-        container.appendChild(div);
+        const divs = document.createElement('div');
+        divs.classList.add('gridCell');
+        container.appendChild(divs);
     }
+}
+
+const gridCells = document.querySelector('.gridCell');
+
+gridCells.addEventListener('mouseover', func);
+
+function func() {
+    gridCells.style.color = 'blue';
 }
